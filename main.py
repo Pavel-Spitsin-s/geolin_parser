b1110f5dbd8629a20b9de94b8acc575bd7e5b8f4
Pavel-Spitsin-s <66683597+Pavel-Spitsin-s@users.noreply.github.com>
2024-03-15 03:32:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from math import factorial
 import fractions
 
 
-#BIG_N - типа COLUMNS
+# BIG_N - типа COLUMNS
 def make5dim(BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
     res = []
     for l in range(BIG_N):
@@ -17,6 +17,8 @@ def make5dim(BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
                     for k in range(COLS):
                         res[l][i][j][f].append(0)
     return res
+
+
 def to5dim(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
     res = make5dim(BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
     for l in range(BIG_N):
@@ -28,6 +30,8 @@ def to5dim(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
                         indv = i * BIG_ROWS + k
                         res[l][i][j][k][f] = arr[indv][indh]
     return res
+
+
 def cnt_inversion(A):
     c = 0
     for i in range(len(A)):
@@ -35,7 +39,9 @@ def cnt_inversion(A):
             if A[i] > A[j]:
                 c += 1
     return c
-def sym(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed):
+
+
+def calc(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed, isSym):
     DIM = 5
     cnt = factorial(DIM - sum(fixed))
     res = []
@@ -52,48 +58,27 @@ def sym(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed):
                             _ = list(_)
                             curp = []
                             bad = False
-                            for i in range(DIM): 
-                                if fixed[i] and i != _[i]: 
+                            for i in range(DIM):
+                                if fixed[i] and i != _[i]:
                                     bad = True
                                 curp.append(perm[_[i]])
                             if bad: continue
+                            mult = 1
+                            if not isSym and cnt_inversion(_) % 2 != 0:
+                                mult = -1
                             ans += arr[curp[0]][curp[1]][curp[2]][curp[3]][curp[4]]
+
                         res[bn][bc][br][r][c] = fractions.Fraction(ans, cnt)
     return res
-def asym(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed):
-    DIM = 5
-    cnt = factorial(DIM - sum(fixed))
-    res = []
-    SAMPLE_DIM = [i for i in range(DIM)]
-    res = make5dim(BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
-    for bn in range(BIG_N):
-        for bc in range(BIG_ROWS):
-            for br in range(BIG_COLUMNS):
-                for r in range(ROWS):
-                    for c in range(COLS):
-                        perm = [bn, bc, br, r, c]
-                        ans = 0
-                        for _ in permutations(SAMPLE_DIM):
-                            _ = list(_)
-                            curp = []
-                            bad = False
-                            for i in range(DIM): 
-                                if fixed[i] and i != _[i]: 
-                                    bad = True
-                                curp.append(perm[_[i]])
-                            if bad: continue
-                            if cnt_inversion(_) % 2 == 0:
-                                ans += arr[curp[0]][curp[1]][curp[2]][curp[3]][curp[4]]
-                            else:
-                                ans -= arr[curp[0]][curp[1]][curp[2]][curp[3]][curp[4]]
-                        
-                        res[bn][bc][br][r][c] = fractions.Fraction(ans, cnt)
-    return res
+
+
 def pretty_ans(arr):
     res = ""
     for i in arr:
         res += i + ', '
     return res
+
+
 def to_ans2(arr):
     res = []
     for f in arr:
@@ -102,6 +87,8 @@ def to_ans2(arr):
         else:
             res.append(str(f.numerator) + "/" + str(f.denominator))
     return pretty_ans(res)
+
+
 def to_ans(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
     ans = []
     for f in range(BIG_ROWS):
@@ -111,8 +98,19 @@ def to_ans(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
                     for k in range(COLS):
                         ans.append(arr[l][f][i][j][k])
     return to_ans2(ans)
-def get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym):
-    if BIG_N == 1: 
+
+
+def get_res(
+        test: list,  # matrix
+        BIG_N: int,  # superlayer horizontal
+        BIG_ROWS: int,  # layer vertical
+        BIG_COLUMNS: int,  # layer horizontal
+        ROWS: int,
+        COLS: int,
+        FIXED: list,  # list boolean -> fixed[i] = true <=> immutable i
+        isSym: bool  # true if sym else alt
+) -> list:
+    if BIG_N == 1:
         FIXED[0] = True
     if BIG_ROWS == 1:
         FIXED[1] = True
@@ -122,13 +120,14 @@ def get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym):
         FIXED[3] = True
     if COLS == 1:
         FIXED[4] = True
-    test = to5dim(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS) 
-    if isSym:
-        return to_ans(sym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
-    else:
-        return to_ans(asym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
+    test = to5dim(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
+    return to_ans(calc(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS,
+                      COLS)
 
-test = [[4, 5, -4, -4, 3, 6, -1, 5, -1], [0, 1, -3, 0, -4, 0, 1, -4, -1], [6, 5, 5, 0, -5, -6, 5, 0, 5], [6, 6, 4, 5, -5, 1, 1, 1, 2], [4, 0, -4, -5, -3, 3, 3, -5, 3], [-5, -2, 1, -2, 1, 0, -4, -5, -4], [1, 3, -4, 3, -3, -2, 4, 2, -1], [0, 4, 1, 6, -5, 3, -4, 3, 1], [-4, -1, -5, 1, 4, -3, 3, -3, -1]]
+
+test = [[4, 5, -4, -4, 3, 6, -1, 5, -1], [0, 1, -3, 0, -4, 0, 1, -4, -1], [6, 5, 5, 0, -5, -6, 5, 0, 5],
+        [6, 6, 4, 5, -5, 1, 1, 1, 2], [4, 0, -4, -5, -3, 3, 3, -5, 3], [-5, -2, 1, -2, 1, 0, -4, -5, -4],
+        [1, 3, -4, 3, -3, -2, 4, 2, -1], [0, 4, 1, 6, -5, 3, -4, 3, 1], [-4, -1, -5, 1, 4, -3, 3, -3, -1]]
 BIG_N = 1
 BIG_ROWS = 3
 BIG_COLUMNS = 3
@@ -137,14 +136,8 @@ COLS = 3
 FIXED = [True, False, False, False, True]
 print(get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, False))
 
-
 '''
 0 0 2 [0, 1, 2] False
 0 0 2 [2, 1, 0] False
 
 '''
-
-            
-                    
-
-            
