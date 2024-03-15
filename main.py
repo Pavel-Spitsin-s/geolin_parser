435c6045f4c506d536e99f33d2a29b06996b6750
Matveiiy <51031652+Matveiiy@users.noreply.github.com>
2024-03-15 03:23:22 +0300

    Slight refactor

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import fractions
 
 
 #BIG_N - типа COLUMNS
-test = [[-6, -1, 4, -1, 6, 4, -5, 1], [5, 2, -4, -2, 5, -2, -6, -1], [0, 4, 3, -4, -5, 6, 3, 3], [1, -1, -2, 4, 1, -1, -4, 6]]
 def make5dim(BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
     res = []
     for l in range(BIG_N):
@@ -112,15 +111,31 @@ def to_ans(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
                     for k in range(COLS):
                         ans.append(arr[l][f][i][j][k])
     return to_ans2(ans)
-    
-BIG_N = 2
-BIG_ROWS = 2
-BIG_COLUMNS = 2
-ROWS = 2
-COLS = 2
-FIXED = [True, True, False, True, False]
-test = to5dim(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)    
-print(to_ans(asym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS))
+def get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym):
+    if BIG_N == 1: 
+        FIXED[0] = True
+    if BIG_ROWS == 1:
+        FIXED[1] = True
+    if BIG_COLUMNS == 1:
+        FIXED[2] = True
+    if ROWS == 1:
+        FIXED[3] = True
+    if COLS == 1:
+        FIXED[4] = True
+    test = to5dim(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS) 
+    if isSym:
+        return to_ans(sym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
+    else:
+        return to_ans(asym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
+
+test = [[4, 5, -4, -4, 3, 6, -1, 5, -1], [0, 1, -3, 0, -4, 0, 1, -4, -1], [6, 5, 5, 0, -5, -6, 5, 0, 5], [6, 6, 4, 5, -5, 1, 1, 1, 2], [4, 0, -4, -5, -3, 3, 3, -5, 3], [-5, -2, 1, -2, 1, 0, -4, -5, -4], [1, 3, -4, 3, -3, -2, 4, 2, -1], [0, 4, 1, 6, -5, 3, -4, 3, 1], [-4, -1, -5, 1, 4, -3, 3, -3, -1]]
+BIG_N = 1
+BIG_ROWS = 3
+BIG_COLUMNS = 3
+ROWS = 3
+COLS = 3
+FIXED = [True, False, False, False, True]
+print(get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, False))
 
 
 '''
