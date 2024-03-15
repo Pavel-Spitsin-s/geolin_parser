0a5fd13885e08e076fa040059e7d9a0ad070b9bd
Pavel-Spitsin-s <66683597+Pavel-Spitsin-s@users.noreply.github.com>
2024-03-15 03:49:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from itertools import *
 from math import factorial
 import fractions
+import parser
 
 
 # BIG_N - типа COLUMNS
@@ -41,7 +42,7 @@ def cnt_inversion(A):
     return c
 
 
-def calc(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed, isSym):
+def sym(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed):
     DIM = 5
     cnt = factorial(DIM - sum(fixed))
     res = []
@@ -63,10 +64,37 @@ def calc(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed, isSym):
                                     bad = True
                                 curp.append(perm[_[i]])
                             if bad: continue
-                            mult = 1
-                            if not isSym and cnt_inversion(_) % 2 != 0:
-                                mult = -1
                             ans += arr[curp[0]][curp[1]][curp[2]][curp[3]][curp[4]]
+                        res[bn][bc][br][r][c] = fractions.Fraction(ans, cnt)
+    return res
+
+
+def asym(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, fixed):
+    DIM = 5
+    cnt = factorial(DIM - sum(fixed))
+    res = []
+    SAMPLE_DIM = [i for i in range(DIM)]
+    res = make5dim(BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
+    for bn in range(BIG_N):
+        for bc in range(BIG_ROWS):
+            for br in range(BIG_COLUMNS):
+                for r in range(ROWS):
+                    for c in range(COLS):
+                        perm = [bn, bc, br, r, c]
+                        ans = 0
+                        for _ in permutations(SAMPLE_DIM):
+                            _ = list(_)
+                            curp = []
+                            bad = False
+                            for i in range(DIM):
+                                if fixed[i] and i != _[i]:
+                                    bad = True
+                                curp.append(perm[_[i]])
+                            if bad: continue
+                            if cnt_inversion(_) % 2 == 0:
+                                ans += arr[curp[0]][curp[1]][curp[2]][curp[3]][curp[4]]
+                            else:
+                                ans -= arr[curp[0]][curp[1]][curp[2]][curp[3]][curp[4]]
 
                         res[bn][bc][br][r][c] = fractions.Fraction(ans, cnt)
     return res
@@ -100,16 +128,7 @@ def to_ans(arr, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS):
     return to_ans2(ans)
 
 
-def get_res(
-        test: list,  # matrix
-        BIG_N: int,  # superlayer horizontal
-        BIG_ROWS: int,  # layer vertical
-        BIG_COLUMNS: int,  # layer horizontal
-        ROWS: int,
-        COLS: int,
-        FIXED: list,  # list boolean -> fixed[i] = true <=> immutable i
-        isSym: bool  # true if sym else alt
-) -> list:
+def get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym):
     if BIG_N == 1:
         FIXED[0] = True
     if BIG_ROWS == 1:
@@ -121,23 +140,39 @@ def get_res(
     if COLS == 1:
         FIXED[4] = True
     test = to5dim(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS)
-    return to_ans(calc(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS,
+    if isSym:
+        return to_ans(sym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS,
+                      COLS)
+    else:
+        return to_ans(asym(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED), BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS,
                       COLS)
 
 
-test = [[4, 5, -4, -4, 3, 6, -1, 5, -1], [0, 1, -3, 0, -4, 0, 1, -4, -1], [6, 5, 5, 0, -5, -6, 5, 0, 5],
-        [6, 6, 4, 5, -5, 1, 1, 1, 2], [4, 0, -4, -5, -3, 3, 3, -5, 3], [-5, -2, 1, -2, 1, 0, -4, -5, -4],
-        [1, 3, -4, 3, -3, -2, 4, 2, -1], [0, 4, 1, 6, -5, 3, -4, 3, 1], [-4, -1, -5, 1, 4, -3, 3, -3, -1]]
-BIG_N = 1
-BIG_ROWS = 3
-BIG_COLUMNS = 3
-ROWS = 3
-COLS = 3
-FIXED = [True, False, False, False, True]
-print(get_res(test, BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, False))
+def solve_problems():
+    problem_set = parser.get_results()
+    cnt_of_problems = len(problem_set)
+    for i in range(cnt_of_problems):
+        #BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym
+        BIG_N = int(input(f"Input BIG_N in {i} problem"))
+        BIG_ROWS = int(input(f"Input BIG_ROWS in {i} problem"))
+        BIG_COLUMNS = int(input(f"Input BIG_COLUMNS in {i} problem"))
+        ROWS = int(input(f"Input ROWS in {i} problem"))
+        COLS = int(input(f"Input COLS in {i} problem"))
+        print("Input FIXED: 0 - false, 1 - true")
+        FIXED = list(map(int, input().split()))
+        isSym = int(input("Input 1 if you want sym 0 - if you want asym"))
+        get_res(problem_set[i], BIG_N, BIG_ROWS, BIG_COLUMNS, ROWS, COLS, FIXED, isSym)
+
+
+if __name__ == "__main__":
+    solve_problems()
 
 '''
 0 0 2 [0, 1, 2] False
 0 0 2 [2, 1, 0] False
 
 '''
+
+
+
+
